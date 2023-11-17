07de16cc2cc82c36af2e31a3a22ad5c5c0f4bc74
wulonglong <wulonglong.917@gmail.com>
2023-11-17 17:15:10 +0800

    完成PermissionX库的开发和测试工作

---
+++ library/src/main/java/com/permissionx/lwudev/PermissionX.kt
@@ -0,0 +1,25 @@
+package com.permissionx.lwudev
+
+import androidx.fragment.app.FragmentActivity
+
+/**
+ * Create by: WuLL
+ * Create date: 2023/11/16 17:46
+ */
+object PermissionX {
+
+    private const val TAG = "InvisibleFragment"
+
+    fun request(activity: FragmentActivity, vararg permissions: String, callback: PermissionCallback) {
+        val fragmentManager = activity.supportFragmentManager
+        val existedFragment = fragmentManager.findFragmentByTag(TAG)
+        val fragment = if (existedFragment != null) {
+            existedFragment as InvisibleFragment
+        } else {
+            val invisibleFragment = InvisibleFragment()
+            fragmentManager.beginTransaction().add(invisibleFragment, TAG).commitNow()
+            invisibleFragment
+        }
+        fragment.requestNow(callback, *permissions)
+    }
+}
